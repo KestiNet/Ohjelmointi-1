3f7eefd7659418317412ed6e6f6713aee94f9dc4
EsaHMKesti <63982745+EsaHMKesti@users.noreply.github.com>
2020-12-02 23:23:42 +0000

    Intellij idea commit

---
+++ Demot/src/Demo8/Toistorakenteet.java
@@ -0,0 +1,13 @@
+package Demo8;
+
+import java.util.Scanner;
+
+public class Toistorakenteet {
+
+    public static double [] kysyLuvut ()
+}
+
+
+public static void main(String[] args){
+    double[] luvut = kysyLuvut("Anna kolme lukua", "luku", 3, 1);
+}
