f75cc7101d4fb461f5581b56bfefc3d425d5cf36
EsaHMKesti <63982745+EsaHMKesti@users.noreply.github.com>
2020-12-03 00:49:51 +0000

    Demojen tekoa

--- Demot/src/Demo8/Toistorakenteet.java
+++ Demot/src/Demo8/Toistorakenteet.java
@@ -1,13 +1,28 @@
 package Demo8;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
+/**
+ * @author esakesti
+ *
+ */
 public class Toistorakenteet {
 
-    public static double [] kysyLuvut ()
+    /**
+     * @return 
+     * 
+     */
+    public static double [] kysyLuvut {
+    	
+    	return 0;
+    }
 }
 
 
+/**
+ * @param args ei käytössä
+ */
 public static void main(String[] args){
     double[] luvut = kysyLuvut("Anna kolme lukua", "luku", 3, 1);
 }
