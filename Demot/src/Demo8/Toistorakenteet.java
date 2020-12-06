f0b91efa8153772f587a84619c0c8b5a6f45de5c
EsaHMKesti <esakesti@gmail.com>
2020-12-06 23:20:19 +0000

    Stuck with this assignment still

--- Demot/src/Demo8/Toistorakenteet.java
+++ Demot/src/Demo8/Toistorakenteet.java
@@ -11,28 +11,29 @@ public class Toistorakenteet {
 
 
 /**
- * @param luvut
+ * @param luku 
  */
-public static void kysyLuvut(double [] luvut) {
-
-	{
-	    Scanner luku = new Scanner(System.in);
-	    double[] numbers = new double[5];
-
-	    for (int i = 0; i < numbers.length; i++)
-	    {
-	        System.out.println("Anna kolme lukua");
-	        numbers[i] = luku.nextDouble();
-	    }
+public static void kysyLuvut(double [] luku) {
+	
+	
+	for (int i = 0; i < luvut.length; i++) {
+		System.out.println("Anna kolme lukua");
+		luku [i] = kysymys.nextDouble();
 	}
+	
+
+
 }
- 
+
 
 /**
  * @param args ei käytössä
  */
 public static void main(String[] args){
-    double[] luvut = kysyLuvut("Anna kolme lukua", "luku", 3, 1);
+	Scanner kysymys = new Scanner(System.in);
+	System.out.println("Anna kolme lukua");
+	double [] luvut = new double[5];
+
 }
 
 
