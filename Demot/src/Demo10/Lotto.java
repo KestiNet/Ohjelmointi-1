d6521206d009cbc4cc2ad8b71ba6a7df524293b8
EsaHMKesti <esakesti@gmail.com>
2021-02-08 00:49:38 +0000

    Lotto machine works, not good but not terrible

--- Demot/src/Demo10/Lotto.java
+++ Demot/src/Demo10/Lotto.java
@@ -1,57 +1,44 @@
 package Demo10;
 
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-class Main {
-    public static void main (String[] args) {
-        Lotto lotto = new Lotto();
+/**
+ * @author esakesti
+ * @param pallot 
+ * @param rand 
+ * @return 
+ *
+ */
+public class Lotto{
+	
+	/**
 
-        lotto.printLottoNumbers();
+/**
+ * @param args
+ */
+public static void main(String[] args) {
+    
+    ArrayList<Integer> pallot = new ArrayList<Integer>();
+    for (int i=1; i<39; i++) {
+        pallot.add(new Integer(i));
+    }
+    Collections.shuffle(pallot);
+    for (int i=0; i<7; i++) {
+        System.out.println(pallot.get(i));
+    }
+    Collections.shuffle(pallot);
+    for (int i=0; i<3; i++) {
+    	System.out.println("Lisänumerot: " + pallot.get(i));
     }
 }
-
-class Lotto {
-    public void printLottoNumbers() {
-        Integer[] lotto = getSortedLottoNumbers();
-
-        System.out.print(lotto[0]);
-
-        for(int i = 1 ; i < 6 ; i++) {
-            System.out.print(", " + lotto[i]);
-        }
-    }
-
-    private List<Integer> getLottoNumberList() {
-        List<Integer> numberList = getNumberList();
-        List<Integer> lotto;
-
-        Collections.shuffle(numberList);
-
-        lotto = numberList.subList(0, 6);
-
-        return lotto;
-    }
-
-    private Integer[] getSortedLottoNumbers() {
-        List<Integer> lotto = getLottoNumberList();
-        Integer[] result = new Integer[6];
-
-        lotto.toArray(result);
-        Arrays.sort(result);
-
-        return result;
-    }
-
-    private List<Integer> getNumberList() {
-        List<Integer> numberList = new ArrayList<Integer>();
-
-        for(int i = 0 ; i < 45 ; i++) {
-            numberList.add(i + 1);
-        }
-
-        return numberList;
-    }
+    // täytä pallot numeroilla 1-39, esim: pallot.add(4);
+    // sotke pallot  (ks. Collections)
+    // tulosta 7 ekaa "palloa"
+    // tulosta 3 seuraavaa palloa
+    
+    
 }
